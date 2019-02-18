f387ca72304b9e16b8c6fffc9e72944bbab0d1f3
Kevin Krato <kratok08@gmail.com>
2019-02-18 13:54:28 -0700

    Check for inputs

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+//buttons
 const chooseFile = document.querySelector("#choose-file");
 const viewFavorites = document.querySelector("#view-favorites");
 const addToAlbum = document.querySelector("#add-to-album");
+
+//inputs
+const form = document.querySelector('.input-form');
+const titleInput = document.querySelector(`[name=title-input]`);
+const captionInput = document.querySelector(`[name=caption-input]`);
+
+// chooseFile.addEventListener("submit", )
+addToAlbum.addEventListener("click", collectUserInputs);
+
+
+function collectUserInputs(e) {
+	e.preventDefault();
+	checkInputs();
+	function checkInputs() {
+		if (!titleInput.value || !captionInput.value) alert('Please enter all fields');
+	}
+}
+
+//will need to implement check for "Choose File" being populated.
+
+//button should check that title, caption, and file, are all populated.
+//once all categories are entered, append card to card section.
