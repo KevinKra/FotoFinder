49ab7a6be62e885e6bf008937a7871c1c7ad7db0
Kevin Krato <kratok08@gmail.com>
2019-02-18 13:24:22 -0700

    Create querySelectors

---
+++ main.js
@@ -0,0 +1,3 @@
+const chooseFile = document.querySelector("#choose-file");
+const viewFavorites = document.querySelector("#view-favorites");
+const addToAlbum = document.querySelector("#add-to-album");
