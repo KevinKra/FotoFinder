e3591e1f150fa3f0c03a50c184971c0606e600bb
Kevin Krato <kratok08@gmail.com>
2019-02-24 19:25:46 -0700

    Complete remove btn

--- photo.js
+++ photo.js
@@ -10,23 +10,13 @@ class Photo {
 		localStorage.setItem("photos", JSON.stringify(photos));
 	}
 	deleteFromStorage() {
-		//Should remove from localStorage;
+		console.log("Am i being targeted?")
+		const target = totalPhotos.indexOf(this);
+		totalPhotos.splice(target, 1);
+		const stringifyPhotos = JSON.stringify(totalPhotos);
+		localStorage.setItem("photos", stringifyPhotos);
 	}
 	updatePhoto() {
 		//Should update document photo;
 	}
-	appendCard() {
-	cardOutputArea.innerHTML += `
-			<article class="card" data-id="${this.id}">
-			<section>
-				<h2 class="card-title">${this.title}</h2>
-				<img src=${this.file} alt="" class="card-image">
-				<p class="card-paragraph">${this.caption}</p>
-			</section>
-			<footer class="card-footer">
-				<button class="btn-trash"><img class="card-trash" src="icons/delete.svg"></button>
-				<button class="btn-like"><img class="card-favorite" src="icons/favorite.svg"></button>
-			</footer>
-		</article>`
-		}
 }
